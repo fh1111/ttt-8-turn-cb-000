351b15ab555e12c9191d09c93aa3d882ed6be377
Fer Hernandez <fh.1111@gmail.com>
2018-09-12 01:28:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,7 +42,8 @@ def turn(board)
   puts "Please enter 1-9:"
   input=gets
   index=input_to_index(input)
-  valid_move?(board,index)
+  if (!valid_move?(board,index))
+    puts "Please enter 1-9:"
 
 
 end
